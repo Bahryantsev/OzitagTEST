a058f4c22f92f43cda320112f81df73b7fb75127
Bahryantsev <bahryantsev@gmail.com>
2021-11-18 20:18:54 +0300

    finallyUndone

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const select = document.querySelector('select')
 select.classList.add('hidden')
 const optionArr = document.querySelectorAll('option')
 const container = document.createElement('div')
+
 //main window
 const header1 = document.createElement('h2');
 const header2 = document.createElement('h2');
@@ -71,25 +72,26 @@ const getSelect = (arr) => {
             level: optionArr[i].dataset.level
         }
         if (options[i].level === undefined) {
-            options[i].level = '1'
+            options[i].level = 1
         }
     }
     return options
 }
+
+//creating new selec below
 const Form = document.createElement('form')
 const options = getSelect(optionArr)
 inputWrapper = []
 inputForm = []
 const inputFormLabel = []
 customCheckbox = []
-for (let i = 0; i < getSelect(optionArr).length; i++) {
+for (let i = 0; i < options.length; i++) {
     customCheckbox[i] = document.createElement('span')
     customCheckbox[i].classList.add('custom_checkbox')
     inputForm[i] = document.createElement('input')
-    inputForm[i].setAttribute('type', 'checkbox'); inputForm[i].setAttribute('id', options[i].value); inputForm[i].classList.add('inputForm');
-    inputFormLabel[i] = document.createElement('label'); inputFormLabel[i].innerText = options[i].title; inputFormLabel[i].setAttribute('for', options[i].value); inputFormLabel[i].classList.add('inputFormLabel')
+    inputForm[i].setAttribute('type', 'checkbox');  inputForm[i].classList.add('inputForm');
+    inputFormLabel[i] = document.createElement('label'); inputFormLabel[i].innerText = options[i].title; inputFormLabel[i].setAttribute('id', options[i].value); inputFormLabel[i].classList.add('inputFormLabel')
     inputWrapper[i] = document.createElement('div'); inputWrapper[i].classList.add('inputWrapper'); inputWrapper[i].append(inputForm[i], customCheckbox[i], inputFormLabel[i]);
-   
     if(options[i].level === 1){
         inputFormLabel[i].style.paddingLeft = '0'
     }else if(+options[i].level === 2){
@@ -108,50 +110,55 @@ for (let i = 0; i < getSelect(optionArr).length; i++) {
     }
     Form.append(inputWrapper[i])
 }
-zalupa = []
-for(let i = 0; i<inputWrapper.length;i++){
-    zalupa[i] = {
-        id:options[i].level,
-        item: inputWrapper[i],
-        parent: options[i].level-1,
-        children: []
+
+// Алгоритм вложенности. Написал, но он не пригодился
+// arr = []
+// for(let i = 0; i<inputWrapper.length;i++){
+//     arr[i] = {
+//         title: inputWrapper[i],
+//         dataLevel: options[i].level,
+//     }
+// }
+// const normalTree = [];
+
+// function setChildren(children, item) {
+//   if (!children.length) {
+//     children.push(item);
+//     return;
+//   }
+
+//   const last = children[children.length - 1];
+//   if (last.dataLevel == item.dataLevel) {
+//     children.push(item);
+    
+    
+//   }
+//   if (last.dataLevel < item.dataLevel) {
+//     last.children ||= [];
+//     setChildren(last.children, item);
+    
+//   }
+// }
+
+// arr.forEach(item => {
+//   setChildren(normalTree, item);
+// })
+
+// console.log(arr, normalTree)
+
+// function print_list(list, container) {
+//     for (var i = 0; i < list.length; i++) {
+//         if (list[i]?.children?.length > 0) {
+//             var div = document.createElement('div');
+//             list[i].title.appendChild(div);
+//             div.classList.add('inputWrapper')
+//             print_list(list[i].children, div);
         
-    }
-}
-let obj_nested_list = []
-    // debugger;
-function fill_with_children(children_arr, parent_id) {
-    // find all objs with parent "parent"
-    for ( i = 0; i < zalupa.length; i++) {
-        obj = zalupa[i];
-        if (obj.parent === parent_id) { 
-            children_arr.push(obj);
-            obj.children = [];
-            fill_with_children(obj.children, obj.id);
-        }
-    }
-    indx = 0;
-    for (let i = 0; i < zalupa.length; i++){
-        if(zalupa[i].id === 0){
-            indx=indx++
-        }
-        if ( zalupa[i].id > 0 ){
-            object = obj_nested_list[indx]
-            while (object != []){
-                object = object.children
-                
-            }
-            object.push(zalupa[i])
-
-        }
-    }
-    
-    
-}
-
-fill_with_children(obj_nested_list, 0);
-console.log(obj_nested_list)
-
+//         }
+//         container.appendChild(list[i].title);
+//     }
+// }
+// print_list(normalTree, Form);
 
 
 
@@ -171,7 +178,7 @@ modalFooter.classList.add('modalFooter')
 
 modalWindow.append(modalWindowHeader, content, modalFooter)
 
-
+// events events events
 link.addEventListener('click', () => {
     modalWindow.style.pointerEvents = 'auto'
     modalWindow.style.opacity = '1'
@@ -181,7 +188,7 @@ arrowBack.addEventListener('click', () => {
     modalWindow.style.pointerEvents = 'none'
     modalWindow.style.opacity = '0'
     count = [];
-    link.innerText = ('Показать выбранное(' + count.length + ')')
+    link.innerText = ''
     mainInput1.setAttribute('placeholder',  'Код ОКРБ или наименование закупаемой продукции'  )
 })
 
@@ -196,20 +203,19 @@ Search.addEventListener('input', () => {
 })
 
 let count = [];
-for (let i = 0; i < inputWrapper.length; i++) {
+for(let i = 0; i< inputWrapper.length;i++){
     inputWrapper[i].addEventListener('click', () => {
         if (inputForm[i].checked) {
-            inputForm[i].checked = false;
             inputWrapper[i].classList.remove('checked')
             count = count.filter(item=> {
                 return item !== inputFormLabel[i]
             })
             
         } else {
-            inputForm[i].checked = true;
             inputWrapper[i].classList.add('checked')
             count.push(inputFormLabel[i])
         }
+        inputForm[i].checked = !inputForm[i].checked
         if(count[0] === undefined){
             mainInput1.setAttribute('placeholder',  'Код ОКРБ или наименование закупаемой продукции'  )
             mainInput1.style.borderLeft = '1px solid #D9D9D9'
@@ -217,8 +223,11 @@ for (let i = 0; i < inputWrapper.length; i++) {
             mainInput1.setAttribute('placeholder',  count[0].innerText )
             mainInput1.style.borderLeft = '2px solid #035B77'
         }
-        link.innerText = ('Показать выбранное(' + count.length + ')')
-        
+        if (count.length > 0 ){
+            link.innerText = ('Показать выбранное(' + count.length + ')')
+        }else{
+            link.innerText = ''
+        }
     }) 
    
 }
@@ -236,7 +245,7 @@ clearBtn.addEventListener('click', () => {
         item.classList.remove('checked')
     })
     count = [];
-    link.innerText = ('Показать выбранное(' + count.length + ')')
+    link.innerText = ''
     mainInput1.setAttribute('placeholder',  'Код ОКРБ или наименование закупаемой продукции'  )
 })
 
@@ -245,7 +254,6 @@ header1.innerText = 'Тендеры в роли Заказчика'
 header2.innerText = 'Тендеры в роли Поставщика'
 header2.classList.add('container_header')
 header1.classList.add('container_header')
-
 mainInput1.classList.add('main_input')
 mainInput2.classList.add('main_input')
 mainInput1.addEventListener('click',()=>{
@@ -263,7 +271,6 @@ mainInput2.readOnly = true
 mainInput1.setAttribute('placeholder',  'Код ОКРБ или наименование закупаемой продукции'  )
 
 link.classList.add('link')
-link.innerText = ('Показать выбранное(' + count.length + ')')
 
 item1.classList.add('item')
 item2.classList.add('item')
@@ -275,6 +282,7 @@ container.append(item1, item2)
 
 
 //appending main elements to body below
+
 document.body.append(container)
 document.body.append(modalWindow)
 
